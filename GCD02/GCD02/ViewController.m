513f700373f974f927e481292fc51a3f6b8dc1a2
chenyufeng <503434355@qq.com>
2016-01-11 21:46:05 +0800

    format code

--- GCD02/GCD02/ViewController.m
+++ GCD02/GCD02/ViewController.m
@@ -9,7 +9,6 @@
 #import "ViewController.h"
 #import "GCD.h"
 
-
 @interface ViewController ()
 
 @end
@@ -17,38 +16,24 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
 
+  [super viewDidLoad];
   NSLog(@"开始启动");
-  
   //NSThread方式的延时执行操作；
   [self performSelector:@selector(threadEvent:)
              withObject:self
              afterDelay:2.f];
-  
   //NSThread可以取消延时操作；
 //  [NSObject cancelPreviousPerformRequestsWithTarget:self];
-  
-  
   //GCD方式实现延迟；
   //主线程；
   [GCDQueue executeInMainQueue:^{
     NSLog(@"GCD实现延迟操作");
   } afterDelaySecs:2.f];
-  
-
-  
-  
 }
 
 - (void)threadEvent:(id)sender{
-
   NSLog(@"NSThread实现延时操作");
-
 }
-
-
-
-
 
 @end
