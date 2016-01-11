ec41018a2a2658aa4541e23939482392538df13d
chenyufeng <503434355@qq.com>
2016-01-11 21:47:04 +0800

    format code

--- GCD03/GCD03/ViewController.m
+++ GCD03/GCD03/ViewController.m
@@ -16,23 +16,21 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+
   [super viewDidLoad];
-  
-  
   //初始化线程组；
   GCDGroup *group = [[GCDGroup alloc] init];
-  
   //创建一个线程队列；
   GCDQueue *queue = [[GCDQueue alloc] initConcurrent];
-  
   //让线程在group中执行；(线程1)
   [queue execute:^{
+
     sleep(1);//延迟1s；
     NSLog(@"线程1执行完毕");
   } inGroup:group];
-  
   //让线程在group中执行；（线程2）
   [queue execute:^{
+
     sleep(3);//延迟3s；
     NSLog(@"线程2执行完毕");
   } inGroup:group];
@@ -41,10 +39,6 @@
   [queue notify:^{
     NSLog(@"线程3执行完毕");
   } inGroup:group];
-  
-  
 }
-
-
 
 @end
